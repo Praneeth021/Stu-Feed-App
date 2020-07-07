164da91326bcd7e4ff37c7455a7a5416fbb11993
Mahitha20 <kothapallymahitha@gmail.com>
2020-07-07 20:14:49 +0530

    save

--- app/src/main/java/com/example/stufeed/MainActivity.java
+++ app/src/main/java/com/example/stufeed/MainActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  Button loginbtn,registerbtn;
+    Button loginbtn;
+    Button registerbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
