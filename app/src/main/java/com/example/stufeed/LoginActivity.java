e6885f6b6ccb1df1874ac64d1c9a117c380591d9
Praneeth021 <kadampallypraneeth987@gmail.com>
2020-07-07 19:49:17 +0530

    Login Module

--- app/src/main/java/com/example/stufeed/LoginActivity.java
+++ app/src/main/java/com/example/stufeed/LoginActivity.java
@@ -3,12 +3,81 @@ package com.example.stufeed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText rollno;
+    private EditText password;
+
+    private RequestQueue queue;
+    private Button loginbtn;
+    private JsonObjectRequest loginRequest;
+
+    static String acc_tkn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        queue= Volley.newRequestQueue(this);
+
+        rollno = findViewById(R.id.rollno1);
+        password = findViewById(R.id.password1);
+
+        loginbtn = findViewById(R.id.login);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String rno=rollno.getText().toString();
+                String pass=rollno.getText().toString();
+
+
+                JSONObject data = new JSONObject();
+
+                try {
+                    data.put("rollno",rno);
+                    data.put("password",pass);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String URL="https://stufeed.herokuapp.com/login";
+                loginRequest=new JsonObjectRequest(Request.Method.POST, URL, data, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            acc_tkn = response.getString("access_token");
+                            Toast.makeText(getApplicationContext(), acc_tkn, Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                queue.add(loginRequest);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/stufeed/MainActivity.java
+++ app/src/main/java/com/example/stufeed/MainActivity.java
@@ -2,36 +2,42 @@ package com.example.stufeed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  Button loginbtn,registerbtn;
+    private Button loginbtn,registerbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginbtn=findViewById(R.id.login);
-        registerbtn=findViewById(R.id.register);
+
+        loginbtn = findViewById(R.id.login);
+
+        registerbtn = findViewById(R.id.register);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
 
+
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(MainActivity.this,RegistrationActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 }
